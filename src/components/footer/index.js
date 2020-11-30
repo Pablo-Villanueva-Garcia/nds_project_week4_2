65b0214a725be70480a1b13fa4891f1237d77a9c
Pablo-Villanueva-Garcia <pablovillanuevagarcia88@gmail.com>
2020-11-30 12:54:51 +0100

    first commit

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,7 +4,7 @@ import './footer.css';
 const Footer = () => {
   return(
     <div>
-    <footer className="xs100 flex just_center ali_end footercolor p20 absoluto" >
+    <footer className="xs100 flex just_center ali_end footercolor p20 absoluto footerposition" >
     <a>THIS IS A FOOTER</a>
     </footer>
     </div>
